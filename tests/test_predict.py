92f54d73766a87075376f101da13d10e982ab113
Cangar00 <14354584+Cangar00@users.noreply.github.com>
2026-05-08 10:43:24 +0800

    update test_predict.py

--- tests/test_predict.py
+++ tests/test_predict.py
@@ -1,15 +1,17 @@
-import glob
 import os
-from decti import DeCTI
+from decti import DeCTI, init_model, init_manager
 
 
 def main():
 
-    a = DeCTI(data_manager='csst_msc_sim', model_param='testdir/trained_model.param.pkl', model_state='testdir/trained_model.state.pth')
+    root = os.path.dirname(os.path.abspath(__file__))
 
-    flist1 = glob.glob('cti_v3.3.0/cti/10100059069/CSST_*_WIDE_*.fits')
-    flist2 = [os.path.join('testdir/output', os.path.basename(f)) for f in flist1]
+    model, params = init_model(seq_len=9232)
+    data_manager = init_manager("csst_msc_sim")
+    a = DeCTI(model, data_manager, state_path=os.path.join(root, 'data', 'trained_model.pth'))
 
+    flist1 = [os.path.join(root, 'data', 'csst_1_cti.fits.gz')]
+    flist2 = [os.path.join('testdir/output', os.path.basename(f).replace('.gz', '')) for f in flist1]
     a.batch_predict(flist1, flist2)
 
 
